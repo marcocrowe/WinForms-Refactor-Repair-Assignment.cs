a3e68740fc9c8abf8bf1933879ee2f058b89763b
Mark Crowe <66536097+marcocrowe@users.noreply.github.com>
2024-08-31 12:41:30 +0100

    Task 4

--- AllergenGroupForm.cs
+++ AllergenGroupForm.cs
@@ -1,6 +1,7 @@
 ﻿using Lab_3;
 using System;
 using System.Windows.Forms;
+using System.Linq;
 
 namespace AllergyInfoApp;
 public partial class AllergenGroupForm : Form
@@ -13,7 +14,7 @@ public partial class AllergenGroupForm : Form
     }
     private void SetupDataForAllergensTab()
     {
-        foreach(Allergens a in form1.ListAllergens)
+        foreach (Allergens a in form1.ListAllergens)
         {
             listView1.Items.Add(new ListViewItem([a.Code, a.Name]));
         }
@@ -23,14 +24,14 @@ public partial class AllergenGroupForm : Form
     private void AllergenGroupForm_Load(object sender, EventArgs e) => SetupDataForAllergensTab();
     private void btnAllergensDelete_Click(object sender, EventArgs e)
     {
-        foreach(ListViewItem x in listView1.SelectedItems)
+        foreach (ListViewItem x in listView1.SelectedItems)
         {
             int allergenNameColumnIndex = 1; // Assuming the allergen name is in the second column
             string allergenName = x.SubItems[allergenNameColumnIndex].Text; // Assuming the allergen name is in the second column
             string boxText = $"Are you sure you want to delete the allergen '{allergenName}'?";
             DialogResult result = MessageBox.Show(boxText, "Confirm Delete", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
 
-            if(result != DialogResult.Yes)
+            if (result != DialogResult.Yes)
                 break;
 
             form1.ListAllergens.RemoveAt(x.Index);
@@ -43,7 +44,7 @@ public partial class AllergenGroupForm : Form
     private void btnAllergensAdd_Click(object sender, EventArgs e)
     {
         //TODO:Task 4
-        if(btnAllergensAdd.Text == "Add")
+        if (btnAllergensAdd.Text == "Add")
         {
             btnAllergensAdd.Text = "Save";
             allergensCodeTextBox.Text = "";
@@ -53,11 +54,19 @@ public partial class AllergenGroupForm : Form
         }
         else
         {
-            if(String.IsNullOrWhiteSpace(allergensCodeTextBox.Text))
+            if (String.IsNullOrWhiteSpace(allergensCodeTextBox.Text))
             {
                 allergensCodeTextBox.Text = $"000{form1.ListAllergens.Count}";
                 allergensNameTextBox.Text = $"Test Allergen {form1.ListAllergens.Count}";
             }
+
+            bool codeExists = form1.ListAllergens.Exists(allergen => allergen.Code == allergensCodeTextBox.Text);
+            if (codeExists)
+            {
+                MessageBox.Show("The allergen code already exists. Please enter a unique code.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
 
             form1.ListAllergens.Add(new Allergens(allergensCodeTextBox.Text, allergensNameTextBox.Text));
 
@@ -73,7 +82,7 @@ public partial class AllergenGroupForm : Form
     }
     private void listView1_SelectedIndexChanged(object sender, EventArgs e)
     {
-        foreach(ListViewItem x in listView1.SelectedItems)
+        foreach (ListViewItem x in listView1.SelectedItems)
         {
             allergensCodeTextBox.Text = x.SubItems[0].Text;
             allergensNameTextBox.Text = x.SubItems[1].Text;
@@ -81,7 +90,7 @@ public partial class AllergenGroupForm : Form
     }
     private void btnAllergensEdit_Click(object sender, EventArgs e)
     {
-        if(btnAllergensEdit.Text == "Edit")
+        if (btnAllergensEdit.Text == "Edit")
         {
             btnAllergensEdit.Text = "Save";
             allergensCodeTextBox.Enabled = true;
@@ -89,7 +98,7 @@ public partial class AllergenGroupForm : Form
         }
         else
         {
-            foreach(ListViewItem x in listView1.SelectedItems)
+            foreach (ListViewItem x in listView1.SelectedItems)
             {
                 form1.ListAllergens.RemoveAt(x.Index);
                 form1.ListAllergens.Add(new Allergens(this.allergensCodeTextBox.Text, this.allergensCodeTextBox.Text));
